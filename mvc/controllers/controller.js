d95a43e926fdb88d15718505de48643e3b33fd8f
daniellamarr <danielchidiebele@gmail.com>
2018-08-24 00:43:50 +0100

    [Feature #159972977] Add endpoint to get one question

--- mvc/controllers/controller.js
+++ mvc/controllers/controller.js
@@ -4,6 +4,11 @@ class Controller {
     getAllQuestions = (req, res) => {
         res.send(questions);
     }
+    getOneQuestion = (req, res) => {
+        const question = questions.find(c => c.id === parseInt(req.params.id));
+        if (!question) res.status(404).send('This question may have been deleted');
+        res.send(question);
+    }
 }
 
 export default new Controller;
